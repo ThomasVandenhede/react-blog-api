f7027ca8c5b846eb3574d50b3ee19d660170d74f
ThomasVandenhede <thomasvandenhede@hotmail.com>
2019-07-31 09:49:03 +0200

    Sort chat messages by time of creation, in ascending order

--- src/api/chat/controller.js
+++ src/api/chat/controller.js
@@ -3,6 +3,7 @@ import { ChatMessage } from ".";
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   ChatMessage.find(query, select, cursor)
+    .sort({ createdAt: 1 })
     .then(chatMessages => chatMessages.map(chatMessage => chatMessage.view()))
     .then(success(res))
     .catch(next);
